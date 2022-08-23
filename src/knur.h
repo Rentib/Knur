2e8724b5f061e641bfa92d5a7a66ad78baeaa7e2
Rentib <sbitner420@tutanota.com>
2022-08-23 10:54:57 +0200

    Makefile, util

---
+++ src/knur.h
@@ -0,0 +1,63 @@
+/*
+  Knur, a chess engine.
+  Copyright (C) 2022 Stanisław Bitner <sbitner420@tutanota.com>
+
+  Knur is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Knur is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef KNUR_H_
+#define KNUR_H_
+
+typedef enum {
+  WHITE, BLACK
+} Color;
+
+typedef enum {
+  PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, NONE
+} PieceType;
+
+typedef enum {
+  SQ_A8, SQ_B8, SQ_C8, SQ_D8, SQ_E8, SQ_F8, SQ_G8, SQ_H8,
+  SQ_A7, SQ_B7, SQ_C7, SQ_D7, SQ_E7, SQ_F7, SQ_G7, SQ_H7,
+  SQ_A6, SQ_B6, SQ_C6, SQ_D6, SQ_E6, SQ_F6, SQ_G6, SQ_H6,
+  SQ_A5, SQ_B5, SQ_C5, SQ_D5, SQ_E5, SQ_F5, SQ_G5, SQ_H5,
+  SQ_A4, SQ_B4, SQ_C4, SQ_D4, SQ_E4, SQ_F4, SQ_G4, SQ_H4,
+  SQ_A3, SQ_B3, SQ_C3, SQ_D3, SQ_E3, SQ_F3, SQ_G3, SQ_H3,
+  SQ_A2, SQ_B2, SQ_C2, SQ_D2, SQ_E2, SQ_F2, SQ_G2, SQ_H2,
+  SQ_A1, SQ_B1, SQ_C1, SQ_D1, SQ_E1, SQ_F1, SQ_G1, SQ_H1,
+  SQ_NONE
+} Square;
+
+typedef enum {
+  Rank1, Rank2, Rank3, Rank4, Rank5, Rank6, Rank7, Rank8
+} Rank;
+
+typedef enum {
+  FileA, FileB, FileC, FileD, FileE, FileF, FileG, FileH
+} File;
+
+typedef enum {
+  NORTH = -8,
+  EAST  =  1,
+  SOUTH =  8,
+  WEST  = -1,
+  NORTH_EAST = NORTH + EAST,
+  SOUTH_EAST = SOUTH + EAST,
+  SOUTH_WEST = SOUTH + WEST,
+  NORTH_WEST = NORTH + WEST,
+  NORTH_NORTH = NORTH + NORTH,
+  SOUTH_SOUTH = SOUTH + SOUTH,
+} Direction;
+
+#endif /* KNUR_H_ */
---
+++ src/util.c
@@ -0,0 +1,44 @@
+/*
+  Knur, a chess engine.
+  Copyright (C) 2022 Stanisław Bitner <sbitner420@tutanota.com>
+
+  Knur is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Knur is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+die(const char *fmt, ...)
+{
+  va_list ap;
+
+  va_start(ap, fmt);
+  fprintf(stderr, fmt, ap);
+  va_end(ap);
+
+  fputc('\n', stderr);
+
+  exit(1);
+}
+
+void *
+emalloc(size_t size)
+{
+  void *p;
+  if (!(p = malloc(size)))
+    die("Knur: cannot allocate memory");
+  return p;
+}
