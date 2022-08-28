f1e3e4334425fb6e39bc3f62ffeb489312a34cf4
Rentib <sbitner420@tutanota.com>
2022-08-28 11:06:21 +0200

    SEARCH: pv storing using PV-List on the Stack

--- src/search.c
+++ src/search.c
@@ -17,29 +17,67 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
 #include "evaluate.h"
 #include "knur.h"
 #include "movegen.h"
 #include "position.h"
 #include "search.h"
+#include "util.h"
 
 #define INFINITY         69000
 #define CHECKMATE        32000
 #define STALEMATE        0
+#define MAX_PLY          64
 
-static int negamax(Position *pos, int alpha, int beta, int depth);
+/** \typedef PV
+ * Defines structure PV.
+ */
+typedef struct PV PV;
+
+/** \struct PV
+ * A structure for storing principle variation.
+ * Dynamic memory management seems to be faster in this case.
+ * When running search for 2'323'511'178 nodes
+ * moves[64] -> 193427ms
+ * moves*    -> 168569ms
+ */
+struct PV {
+  /**{*/
+  size_t len; /**< Number of pv moves stored; */
+  Move *line; /**< Array for storing pv moves. */
+  /**}*/
+};
+
+static const char *mtstr(Move m);
+static int negamax(Position *pos, PV *pv, int alpha, int beta, int depth);
+static PV *pv_create(size_t len);
+static void pv_free(PV *pv);
 
 SearchInfo info;
 
+static const char *
+mtstr(Move m)
+{
+  const char ptc[6] = { 'p', 'n', 'b', 'r', 'q', 'k' };
+  static char res[6];
+  sprintf(res, "%c%c%c%c%c",
+      'a' + (FROM_SQ(m) & 7), '8' - (FROM_SQ(m) >> 3),
+      'a' + (  TO_SQ(m) & 7), '8' - (  TO_SQ(m) >> 3),
+      TYPE_OF(m) == PROMOTION ? ptc[PROMOTION_TYPE(m)] : '\0');
+  return res;
+}
+
 static int
-negamax(Position *pos, int alpha, int beta, int depth)
+negamax(Position *pos, PV *pv, int alpha, int beta, int depth)
 {
   int value;
   Move *m, *last, move_list[256];
   U64 checkers = attackers_to(pos, pos->ksq[pos->turn], ~pos->empty)
                & pos->color[!pos->turn];
-  int legalmove = 0;
+  unsigned legalmove = 0;
+  PV *new_pv;
 
   if (!depth) {
     if (!checkers)
@@ -47,6 +85,7 @@ negamax(Position *pos, int alpha, int beta, int depth)
     depth = 1;
   }
 
+  new_pv = pv_create(MAX_PLY - pos->ply);
   last = generate_moves(GT_ALL, move_list, pos);
 
   for (m = move_list; m != last; m++) {
@@ -54,15 +93,23 @@ negamax(Position *pos, int alpha, int beta, int depth)
       continue;
     legalmove++;
     do_move(pos, *m);
-    value = -negamax(pos, -beta, -alpha, depth - 1);
+    value = -negamax(pos, new_pv, -beta, -alpha, depth - 1);
     undo_move(pos, *m);
 
-    if (value >= beta)
+    if (value >= beta) {
+      pv_free(new_pv);
       return value;
+    }
 
-    if (value > alpha)
+    if (value > alpha) {
+      pv->line[0] = *m;
+      pv->len = new_pv->len + 1;
+      memcpy(pv->line + 1, new_pv->line, new_pv->len * sizeof(Move));
       alpha = value;
+    }
   }
+
+  pv_free(new_pv);
 
   if (!legalmove)
     return checkers ? pos->ply - CHECKMATE : STALEMATE;
@@ -70,44 +117,44 @@ negamax(Position *pos, int alpha, int beta, int depth)
   return alpha;
 }
 
-static inline const char *
-mtstr(Move m)
+static PV *
+pv_create(size_t len)
 {
-  const char ptc[6] = { 'p', 'n', 'b', 'r', 'q', 'k' };
-  static char res[6];
-  sprintf(res, "%c%c%c%c%c", 
-      'a' + (FROM_SQ(m) & 7), '8' - (FROM_SQ(m) >> 3),
-      'a' + (  TO_SQ(m) & 7), '8' - (  TO_SQ(m) >> 3),
-      TYPE_OF(m) == PROMOTION ? ptc[PROMOTION_TYPE(m)] : '\0');
-  return res;
+  PV *pv = emalloc(sizeof(PV));
+  pv->len = 0;
+  pv->line = emalloc(len * sizeof(Move));
+  return pv;
+}
+
+static void
+pv_free(PV *pv)
+{
+  if (pv) {
+    free(pv->line);
+    free(pv);
+  }
 }
 
 void
 search(Position *pos)
 {
-  int bestvalue = -CHECKMATE, value;
-  int alpha = -INFINITY, beta = INFINITY;
+  int score, alpha = -INFINITY, beta = INFINITY;
+  unsigned start = gettime(), i;
   Move bestmove = MOVE_NONE;
-  Move *m, *last, move_list[256];
+  PV *pv = pv_create(MAX_PLY);
 
   pos->ply = 0;
-  last = generate_moves(GT_ALL, move_list, pos);
 
-  for (m = move_list; m != last; m++) {
-    if (!is_legal(pos, *m))
-      continue;
-    do_move(pos, *m);
-    value = -negamax(pos, -beta, -alpha, info.depth - 1);
-    undo_move(pos, *m);
+  score = negamax(pos, pv, alpha, beta, info.depth);
 
-    if (value > alpha)
-      alpha = value;
-
-    if (value > bestvalue) {
-      bestvalue = value;
-      bestmove = *m;
-    }
-  }
+  bestmove = *pv->line;
+  printf("info depth %d score cp %d nodes %lu time %d pv",
+      info.depth, score, 0UL, gettime() - start);
+  for (i = 0; i < pv->len; i++)
+    printf(" %s", mtstr(pv->line[i]));
+  printf("\n");
 
   printf("bestmove %s\n", mtstr(bestmove));
+
+  pv_free(pv);
 }
