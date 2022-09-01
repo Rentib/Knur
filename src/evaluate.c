781d46f4cd3f703fbe2bab9996d77301c8e6c915
Rentib <sbitner420@tutanota.com>
2022-09-01 16:08:48 +0200

    EVALUATE: refactor

--- src/evaluate.c
+++ src/evaluate.c
@@ -16,29 +16,24 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <byteswap.h>
+
 #include "bitboards.h"
-#include "knur.h"
 #include "evaluate.h"
+#include "knur.h"
 #include "position.h"
 
-/* 
- * Knur uses a very simple evaluation method:
- * - material
- * - mobility
- * - piece square tables
- * - king safety
- * - outposts
- * - bishop pair
- * - open files
- * - passed pawns
- * - doubled pawns
- * - isolated pawns
- */
-
-#define FLIP(square)        ((square) ^ 56)
+#define FLIP(bitboard)        bitboard = bswap_64(bitboard)
+#define SQRANK(square)        ((square) >> 3)
+#define SQFILE(square)        ((square) & 7)
+#define SQFLIP(square)        ((square) ^ 56)
 
 typedef enum { MID_GAME, END_GAME } GamePhase;
-static GamePhase phase;
+
+static int eval_pawns(const Color side, const Position *pos);
+static int eval_knights(const Color side, const Position *pos);
+static int eval_bishops(const Color side, const Position *pos);
+static int eval_rooks(const Color side, const Position *pos);
 
 /* Piece Square Tables */
 static const int pawn_pcsqt[64][2] = {
@@ -95,30 +90,189 @@ static const int king_pcsqt[64][2] = {
   { 10,-50}, { 20,-20}, { 20,-20}, {-10,-20}, {  0,-20}, {-10,-20}, { 20,-20}, { 20,-50},
 };
 
-static U64 rank_mask[8];       /* [Rank] */
-static U64 file_mask[8];       /* [File] */
-static U64 adj_file_mask[8];   /* [File] */
-static U64 passed_mask[2][64]; /* [Color][Square] */
+static GamePhase phase;
+static int pawn_cnt[2]; /* side */
 
+/* bonuses in centipawns */
 static const int passed[8][2]   = {{0,0},{53,100},{31,45},{15,22},{8,14},{5,9},{2,5},{0,0}};
 static const int isolated[2] = { -10, -25 };
 static const int doubled[2] = { -15, -25 };
-
 static const int outpost[2] = { 15, 9 };
 static const int knight_adj[9] = { -10, -8, -6, -4, -2, 0, 2, 4, 8 };
-
 static const int bishop_pair[2] = { 20, 40 };
-
 static const int open_file[2]   = { 10, 20 }; /* semiopen, open */
 static const int king_file[2]   = { 10, 20 };
 static const int rook_adj[9] = { 13, 12, 10, 7, 3, 0, -3, -6, -9 };
-
 static const int king_shield[2] = { 2, 7 };
+
+/* helper masks */
+U64 rank_mask[8];
+U64 file_mask[8];
+U64 adj_file_mask[8];
+U64 passed_mask[64];
+
+static int
+eval_pawns(const Color side, const Position *pos)
+{
+  int value = 0;
+  Square sq;
+  U64 mask;
+  U64 allied_pawns = pos->color[side] & pos->piece[PAWN];
+  U64 enemy_pawns = pos->color[!side] & pos->piece[PAWN];
+
+  pawn_cnt[side] = POPCOUNT(allied_pawns);
+
+  if (!allied_pawns)
+    return 0;
+
+  if (side == BLACK) {
+    FLIP(allied_pawns);
+    FLIP(enemy_pawns);
+  }
+
+  for (mask = allied_pawns; mask; ) {
+    sq = pop_lsb(&mask);
+    value += pawn_pcsqt[sq][phase];
+
+    /* passed */
+    if (!(passed_mask[sq] & enemy_pawns))
+      value += passed[SQRANK(sq)][phase];
+
+    /* doubled */
+    if (file_mask[SQFILE(sq)] & passed_mask[sq] & allied_pawns)
+      value += doubled[phase];
+
+    /* isolated */
+    if (!(adj_file_mask[SQFILE(sq)] & allied_pawns))
+      value += isolated[phase];
+  }
+
+  return value;
+}
+
+static int
+eval_knights(const Color side, const Position *pos)
+{
+  int value = 0;
+  Square sq;
+  U64 allies = pos->color[side];
+  U64 enemies = pos->color[!side];
+  U64 knights = allies & pos->piece[KNIGHT];
+  U64 allied_pawns = allies & pos->piece[PAWN];
+  U64 enemy_pawns = enemies & pos->piece[PAWN];
+
+  if (!knights)
+    return 0;
+
+  if (side == BLACK) {
+    FLIP(allies);
+    FLIP(enemies);
+    FLIP(knights);
+    FLIP(allied_pawns);
+    FLIP(enemy_pawns);
+  }
+
+  while (knights) {
+    sq = pop_lsb(&knights);
+    value += knight_pcsqt[sq][phase];
+
+    /* mobility */
+    value += POPCOUNT(attacks_bb(KNIGHT, sq, 0) & ~allies);
+
+    /* outpost */
+    if (!(adj_file_mask[SQFILE(sq)] & passed_mask[sq] & enemy_pawns))
+      value += outpost[phase];
+
+    /* adjust value depending on number of pawns */
+    value += knight_adj[pawn_cnt[side]];
+  }
+
+  return value;
+}
+
+static int
+eval_bishops(const Color side, const Position *pos)
+{
+  int value = 0;
+  Square sq;
+  U64 allies = pos->color[side];
+  U64 enemies = pos->color[!side];
+  U64 bishops = allies & pos->piece[BISHOP];
+  U64 enemy_pawns = enemies & pos->piece[PAWN];
+
+  if (!bishops)
+    return 0;
+
+  if (side == BLACK) {
+    FLIP(allies);
+    FLIP(enemies);
+    FLIP(bishops);
+    FLIP(enemy_pawns);
+  }
+
+  /* bishop pair */
+  if (bishops & (bishops - 1))
+    value += bishop_pair[phase];
+
+  while (bishops) {
+    sq = pop_lsb(&bishops);
+    value += bishop_pcsqt[sq][phase];
+
+    /* mobility */
+    value += POPCOUNT(attacks_bb(BISHOP, sq, allies | enemies) & ~allies);
+
+    /* outpost */
+    if (!(adj_file_mask[SQFILE(sq)] & passed_mask[sq] & enemy_pawns))
+      value += outpost[phase];
+  }
+
+  return value;
+}
+
+static int
+eval_rooks(const Color side, const Position *pos)
+{
+  int value = 0;
+  Square sq;
+  U64 allies = pos->color[side];
+  U64 rooks = allies & pos->piece[ROOK];
+  Square ksq = pos->ksq[!side]; /* enemy king */
+
+  if (!rooks)
+    return 0;
+
+  if (side == BLACK) {
+    FLIP(allies);
+    FLIP(rooks);
+    ksq = SQFLIP(ksq);
+  }
+
+  while (rooks) {
+    sq = pop_lsb(&rooks);
+    value += rook_pcsqt[sq];
+
+    /* (semi) open file */
+    if (!(file_mask[SQFILE(sq)] & pos->piece[PAWN])) /* pos->piece doesnt need flip */
+      value += open_file[1];
+    else if (!(file_mask[SQFILE(sq)] & pos->piece[PAWN] & pos->color[side]))
+      value += open_file[0];
+
+    /* same file/rank as enemy king */
+    if (SQFILE(sq) == SQFILE(ksq) || SQRANK(sq) == SQRANK(ksq))
+      value += king_file[phase];
+
+    /* adjust value depending on number of pawns */
+    value += rook_adj[pawn_cnt[side]];
+  }
+
+  return value;
+}
 
 void
 evaluation_init(void)
 {
   Square sq; File f; Rank r;
+
   for (f = 0; f < 8; f++) {
     for (r = 0; r < 8; r++) {
       SET_BIT(file_mask[f], 8 * r + f);
@@ -127,136 +281,32 @@ evaluation_init(void)
     adj_file_mask[f] = shift(WEST, file_mask[f]) | shift(EAST, file_mask[f]);
   }
 
-  for (sq = SQ_A8; sq <= SQ_H1; sq++) {
-    passed_mask[WHITE][sq] = (adj_file_mask[sq & 7] | file_mask[sq & 7])
-                           &  ((1ULL << sq) - 1) & ~rank_mask[sq >> 3];
-    passed_mask[BLACK][sq] = (adj_file_mask[sq & 7] | file_mask[sq & 7])
-                           & ~((1ULL << sq) - 1) & ~rank_mask[sq >> 3];
-  }
+  for (sq = SQ_A8; sq <= SQ_H1; sq++)
+    passed_mask[sq] = (adj_file_mask[SQFILE(sq)] | file_mask[SQFILE(sq)])
+                    & (GET_BITBOARD(sq) - 1) & ~rank_mask[SQRANK(sq)];
 }
 
 int
 evaluate(const Position *pos)
 {
-  int value       = pos->material[WHITE] - pos->material[BLACK];
-  U64 white_pawns = pos->color[WHITE] & pos->piece[PAWN];
-  U64 black_pawns = pos->color[BLACK] & pos->piece[PAWN];
-  U64 occupancy   = ~pos->empty;
-  U64 mask;
-  Square sq, fsq;
-  int wpanws_cnt = POPCOUNT(white_pawns);
-  int bpanws_cnt = POPCOUNT(black_pawns);
+  int value = 0;
 
-  /* Game Phase */
+  /* probe tt for evaluation */
+
   phase = pos->material[WHITE] + pos->material[BLACK] > 3000 ? MID_GAME : END_GAME;
 
-  /* Pawns */
+  /* material */
+  value += pos->material[WHITE] - pos->material[BLACK];
 
-  for (mask = white_pawns; mask; ) {
-    sq = pop_lsb(&mask);
-    value += pawn_pcsqt[sq][phase];
-    if (!(passed_mask[WHITE][sq] & black_pawns))
-      value += passed[sq >> 3][phase];
-    if (file_mask[sq & 7] & white_pawns & passed_mask[WHITE][sq])
-      value += doubled[phase];
-    if (!(adj_file_mask[sq & 7] & white_pawns))
-      value += isolated[phase];
-  }
+  value += eval_pawns(WHITE, pos) - eval_pawns(BLACK, pos);
+  value += eval_knights(WHITE, pos) - eval_knights(BLACK, pos);
+  value += eval_bishops(WHITE, pos) - eval_bishops(BLACK, pos);
+  value += eval_rooks(WHITE, pos) - eval_rooks(BLACK, pos);
 
-  for (mask = black_pawns; mask; ) {
-    sq = pop_lsb(&mask);
-    fsq = FLIP(sq);
-    value -= pawn_pcsqt[fsq][phase];
-    if (!(passed_mask[BLACK][sq] & white_pawns))
-      value -= passed[fsq >> 3][phase];
-    if (file_mask[sq & 7] & black_pawns & passed_mask[BLACK][sq])
-      value -= doubled[phase];
-    if (!(adj_file_mask[sq & 7] & white_pawns))
-      value -= isolated[phase];
-  }
-
-  /* Knights */
-
-  for (mask = pos->color[WHITE] & pos->piece[KNIGHT]; mask; ) {
-    sq = pop_lsb(&mask);
-    value += knight_pcsqt[sq][phase];
-    value += POPCOUNT(attacks_bb(KNIGHT, sq, occupancy) & ~pos->color[WHITE]);
-    if (!(adj_file_mask[sq & 7] & passed_mask[WHITE][sq] & black_pawns))
-      value += outpost[phase];
-    value += knight_adj[wpanws_cnt];
-  }
-
-  for (mask = pos->color[BLACK] & pos->piece[KNIGHT]; mask; ) {
-    sq = pop_lsb(&mask);
-    value -= knight_pcsqt[FLIP(sq)][phase];
-    value -= POPCOUNT(attacks_bb(KNIGHT, sq, occupancy) & ~pos->color[BLACK]);
-    if (!(adj_file_mask[sq & 7] & passed_mask[BLACK][sq] & white_pawns))
-      value -= outpost[phase];
-    value -= knight_adj[bpanws_cnt];
-  }
-
-  /* Bishops */
-
-  mask = pos->color[WHITE] & pos->piece[BISHOP];
-  if (mask & (mask - 1))
-    value += bishop_pair[phase];
-
-  for (; mask; ) {
-    sq = pop_lsb(&mask);
-    value += bishop_pcsqt[sq][phase];
-    value += POPCOUNT(attacks_bb(BISHOP, sq, occupancy) & ~pos->color[WHITE]);
-    if (!(adj_file_mask[sq & 7] & passed_mask[WHITE][sq] & black_pawns))
-      value += outpost[phase];
-  }
-
-  mask = pos->color[BLACK] & pos->piece[BISHOP];
-  if (mask & (mask - 1))
-    value -= bishop_pair[phase];
-
-  for (; mask; ) {
-    sq = pop_lsb(&mask);
-    value -= bishop_pcsqt[FLIP(sq)][phase];
-    value -= POPCOUNT(attacks_bb(BISHOP, sq, occupancy) & ~pos->color[BLACK]);
-    if (!(adj_file_mask[sq & 7] & passed_mask[BLACK][sq] & white_pawns))
-      value -= outpost[phase];
-  }
-
-  /* Rooks */
-
-  for (mask = pos->color[WHITE] & pos->piece[ROOK]; mask; ) {
-    sq = pop_lsb(&mask);
-    value += rook_pcsqt[sq];
-    if (!(file_mask[sq & 7] & pos->piece[PAWN]))
-      value += open_file[1];
-    else if (!(file_mask[sq & 7] & white_pawns))
-      value += open_file[0];
-    if ((sq & 7) == (pos->ksq[BLACK] & 7)
-    || (sq >> 3) == (pos->ksq[BLACK] >> 3))
-      value += king_file[phase];
-    value += rook_adj[wpanws_cnt];
-  }
-
-  for (mask = pos->color[BLACK] & pos->piece[ROOK]; mask; ) {
-    sq = pop_lsb(&mask);
-    value -= rook_pcsqt[FLIP(sq)];
-    if (!(file_mask[sq & 7] & pos->piece[PAWN]))
-      value -= open_file[1];
-    else if (!(file_mask[sq & 7] & black_pawns))
-      value -= open_file[0];
-    if ((sq & 7) == (pos->ksq[WHITE] & 7)
-    || (sq >> 3) == (pos->ksq[WHITE] >> 3))
-      value -= king_file[phase];
-    value -= rook_adj[bpanws_cnt];
-  }
-
-  /* Queens */
-
-  /* Kings */
-
-  value += king_pcsqt[pos->ksq[WHITE]][phase] - king_pcsqt[FLIP(pos->ksq[BLACK])][phase];
+  value += king_pcsqt[pos->ksq[WHITE]][phase] - king_pcsqt[SQFLIP(pos->ksq[BLACK])][phase];
   value += king_shield[phase] *
-           (POPCOUNT(attacks_bb(KING, pos->ksq[WHITE], 0) & white_pawns) -
-            POPCOUNT(attacks_bb(KING, pos->ksq[BLACK], 0) & black_pawns));
+           (POPCOUNT(attacks_bb(KING, pos->ksq[WHITE], 0) & pos->piece[PAWN] & pos->color[WHITE]) -
+            POPCOUNT(attacks_bb(KING, pos->ksq[BLACK], 0) & pos->piece[PAWN] & pos->color[BLACK]));
 
   return pos->turn == WHITE ? value : -value;
 }
