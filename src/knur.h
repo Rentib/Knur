8fae92e75832e3e989854459f7e5e23eb5a2a134
Rentib <sbitner420@tutanota.com>
2024-07-31 17:16:20 +0200

    feat(knur): basic types

--- src/knur.h
+++ src/knur.h
@@ -1,4 +1,74 @@
 #ifndef KNUR_H_
 #define KNUR_H_
 
+enum color { WHITE, BLACK, COLOR_NB };
+
+enum piece_type { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, PIECE_TYPE_NB };
+
+/* clang-format off */
+enum piece {
+	WHITE_PAWN   =  0, BLACK_PAWN   = 1,
+	WHITE_KNIGHT =  4, BLACK_KNIGHT = 5,
+	WHITE_BISHOP =  8, BLACK_BISHOP = 9,
+	WHITE_ROOK   = 12, BLACK_ROOK   = 13,
+	WHITE_QUEEN  = 16, BLACK_QUEEN  = 17,
+	WHITE_KING   = 20, BLACK_KING   = 21,
+	EMPTY        = 26,
+};
+/* clang-format on */
+
+#define PIECE_TYPE(piece)       ((piece) / 4)
+#define PIECE_COLOR(piece)      ((piece) % 4)
+#define PIECE_MAKE(type, color) ((type) * 4 + (color))
+
+/* clang-format off */
+enum square {
+	SQ_A8, SQ_B8, SQ_C8, SQ_D8, SQ_E8, SQ_F8, SQ_G8, SQ_H8,
+	SQ_A7, SQ_B7, SQ_C7, SQ_D7, SQ_E7, SQ_F7, SQ_G7, SQ_H7,
+	SQ_A6, SQ_B6, SQ_C6, SQ_D6, SQ_E6, SQ_F6, SQ_G6, SQ_H6,
+	SQ_A5, SQ_B5, SQ_C5, SQ_D5, SQ_E5, SQ_F5, SQ_G5, SQ_H5,
+	SQ_A4, SQ_B4, SQ_C4, SQ_D4, SQ_E4, SQ_F4, SQ_G4, SQ_H4,
+	SQ_A3, SQ_B3, SQ_C3, SQ_D3, SQ_E3, SQ_F3, SQ_G3, SQ_H3,
+	SQ_A2, SQ_B2, SQ_C2, SQ_D2, SQ_E2, SQ_F2, SQ_G2, SQ_H2,
+	SQ_A1, SQ_B1, SQ_C1, SQ_D1, SQ_E1, SQ_F1, SQ_G1, SQ_H1,
+	SQ_NB, SQ_NONE,
+};
+/* clang-format on */
+
+#define SQ_FILE(square) ((square) & 7)
+#define SQ_RANK(square) ((square) >> 3)
+#define SQ_FLIP(square) ((square) ^ 56)
+#define SQ_STR(square)                                                         \
+	((square) == SQ_NONE                                                   \
+	     ? "NO"                                                            \
+	     : (char[]){'a' + SQ_FILE(square), '8' - SQ_RANK(square), 0})
+
+enum direction {
+	NORTH = -8,
+	SOUTH = +8,
+	EAST = +1,
+	WEST = -1,
+	NORTH_EAST = NORTH + EAST,
+	NORTH_WEST = NORTH + WEST,
+	SOUTH_EAST = SOUTH + EAST,
+	SOUTH_WEST = SOUTH + WEST,
+	NORTH_NORTH = NORTH + NORTH,
+	SOUTH_SOUTH = SOUTH + SOUTH,
+};
+
+#define ABS(X)        ((X) < 0 ? -(X) : (X))
+#define MAX(A, B)     ((A) > (B) ? (A) : (B))
+#define MIN(A, B)     ((A) < (B) ? (A) : (B))
+
+#define MAX_MOVES      (1024)
+#define MAX_PLY        (64)
+#define STALEMATE      (0)
+#define CHECKMATE      (32000 + MAX_PLY)
+#define UNKNOWN        (CHECKMATE + 1)
+#define MATE_IN(n)     (CHECKMATE - (n))
+#define MATED_IN(n)    (CHECKMATE + (n))
+#define IS_MATE(score) (ABS(score) >= CHECKMATE - MAX_PLY)
+
+#define INLINE static inline __attribute__((always_inline))
+
 #endif /* KNUR_H_ */
