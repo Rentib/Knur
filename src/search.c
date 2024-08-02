0513f8d817457a5dee8c4cea45c6376dd7ace770
Rentib <sbitner420@tutanota.com>
2024-08-02 14:29:55 +0200

    feat(search): time management

--- src/search.c
+++ src/search.c
@@ -3,35 +3,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <threads.h>
-#include <unistd.h>
 
+#include "knur.h"
 #include "position.h"
 #include "search.h"
 #include "util.h"
-
-static atomic_bool running = false, thrd_joined = true;
-static pthread_t thrd;
 
 struct arg {
 	struct position *pos;
 	struct search_limits limits;
 };
 
-static void *search(void *arg)
+static void *search(void *arg);
+static void *time_manager(void *arg);
+
+static atomic_bool running = false, thrd_joined = true;
+static pthread_t thrd;
+
+void *search(void *arg)
 {
 	struct arg *starg = arg;
 	struct position *pos = starg->pos;
 	struct search_limits *limits = &starg->limits;
+	pthread_t manager;
+	int maxdepth = limits->depth;
+
+	if (maxdepth <= 0 || maxdepth > MAX_PLY)
+		maxdepth = MAX_PLY;
 
 	(void)pos;
 	(void)limits;
+
+	if (pthread_create(&manager, nullptr, time_manager, arg))
+		die("pthread_create:");
+	if (pthread_detach(manager))
+		die("pthread_detach:");
 
 	/* TODO: find best move */
 
 	printf("bestmove e2e4\n");
 
-	free(arg);
+	if (pthread_cancel(manager))
+		die("pthread_cancel:");
+
 	running = false;
+	return nullptr;
+}
+
+static void *time_manager(void *arg)
+{
+	struct search_limits *limits = &((struct arg *)arg)->limits;
+	int time = limits->time, inc = limits->inc,
+	    movestogo = limits->movestogo, movetime = limits->movetime;
+
+	free(arg);
+	if (movetime != -1) {
+		movestogo = 1;
+		time = movetime;
+	}
+
+	if (time == -1)
+		return nullptr;
+	movetime = time / movestogo + inc - 50;
+
+	thrd_sleep(&(struct timespec){.tv_sec = movetime / 1000,
+				      .tv_nsec = (movetime % 1000) * 1000000},
+		   nullptr);
+	running = false;
+
 	return nullptr;
 }
 
