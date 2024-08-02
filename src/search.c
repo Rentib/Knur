50e830e8c186337c1b82dbafc4b4904c5f906ec1
Rentib <sbitner420@tutanota.com>
2024-08-02 16:41:48 +0200

    feat(search): negamax with iterative deepening

--- src/search.c
+++ src/search.c
@@ -1,10 +1,13 @@
 #include <pthread.h>
+#include <setjmp.h>
 #include <stdatomic.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <threads.h>
 
+#include "evaluate.h"
 #include "knur.h"
+#include "movegen.h"
 #include "position.h"
 #include "search.h"
 #include "util.h"
@@ -14,11 +17,71 @@ struct arg {
 	struct search_limits limits;
 };
 
+struct search_stack {
+	int ply;
+	enum move move;
+};
+
+static int negamax(struct position *position, struct search_stack *search_stack,
+		   int alpha, int beta, int depth);
 static void *search(void *arg);
 static void *time_manager(void *arg);
 
 static atomic_bool running = false, thrd_joined = true;
 static pthread_t thrd;
+static jmp_buf jbuffer;
+
+int negamax(struct position *pos, struct search_stack *ss, int alpha, int beta,
+	    int depth)
+{
+	int isroot = !ss->ply;
+	int score;
+	int bestscore = -CHECKMATE;
+	enum move bestmove;
+	enum move move_list[256], *last, *m;
+
+	if (!running)
+		longjmp(jbuffer, 1);
+
+	if (!isroot) {
+		if (!depth) {
+			/* TODO: quiescence */
+			if (!pos->st->checkers)
+				return evaluate(pos);
+			return 0;
+		}
+
+		if (ss->ply >= MAX_PLY - 1)
+			return pos->st->checkers ? 0 : evaluate(pos);
+	}
+
+	last = mg_generate(MGT_ALL, move_list, pos);
+	for (m = move_list; m != last; m++) {
+		if (!pos_is_legal(pos, *m))
+			continue;
+
+		ss->move = *m;
+		pos_do_move(pos, *m);
+		score = -negamax(pos, ss + 1, -beta, -alpha, depth - 1);
+		pos_undo_move(pos, *m);
+
+		if (score <= bestscore)
+			continue;
+		bestscore = score;
+		bestmove = *m;
+
+		if (score >= beta)
+			break;
+
+		if (score > alpha)
+			alpha = score;
+	}
+
+	if (isroot)
+		ss->move = bestmove;
+
+	return bestscore;
+}
 
 void *search(void *arg)
 {
@@ -27,21 +90,34 @@ void *search(void *arg)
 	struct search_limits *limits = &starg->limits;
 	pthread_t manager;
 	int maxdepth = limits->depth;
+	struct search_stack search_stack[MAX_PLY + 2] = {0};
+	struct search_stack *ss = search_stack + 2;
+	unsigned i;
+	int depth, score;
 
 	if (maxdepth <= 0 || MAX_PLY <= maxdepth)
 		maxdepth = MAX_PLY;
 
-	(void)pos;
-	(void)limits;
+	/* initialize search stack */
+	for (i = 0; i < MAX_PLY; i++)
+		(ss + i)->ply = i;
 
 	if (pthread_create(&manager, nullptr, time_manager, arg))
 		die("pthread_create:");
 	if (pthread_detach(manager))
 		die("pthread_detach:");
 
-	/* TODO: find best move */
+	for (depth = 1; depth <= maxdepth; depth++) {
+		if (setjmp(jbuffer))
+			break;
 
-	printf("bestmove e2e4\n");
+		score = negamax(pos, ss, -CHECKMATE, CHECKMATE, depth);
+
+		printf("info depth %d ", depth);
+		printf("score cp %d\n", score);
+	}
+
+	printf("bestmove %s\n", MOVE_STR(ss->move));
 
 	if (pthread_cancel(manager))
 		die("pthread_cancel:");
