4aa059117bf6ddb5558a104aabc3893c5c6f1df2
Rentib <sbitner420@tutanota.com>
2024-08-14 01:55:59 +0200

    feat(movepicker): static exchange evaluation and bad captures

--- src/movepicker.c
+++ src/movepicker.c
@@ -1,22 +1,66 @@
 #include "movepicker.h"
+#include "bitboards.h"
 #include "knur.h"
 #include "movegen.h"
 #include "position.h"
 #include "search.h"
 
+static int see(struct position *position, enum move move);
 static void score_captures(struct move_picker *move_picker,
 			   struct position *position);
 static void sort_moves(struct move_picker *move_picker, enum move *begin,
 		       enum move *end);
 
-static int mvv[PIECE_TYPE_NB] = {100, 300, 315, 500, 900, 0, 0};
+static const int mvv[PIECE_TYPE_NB] = {100, 300, 315, 500, 900, 20000, 0};
+
+/* Static Exchange Evaluation - The Swap Algorithm
+ * https://www.chessprogramming.org/SEE_-_The_Swap_Algorithm */
+int see(struct position *pos, enum move move)
+{
+	int gain[32], d = 0;
+	enum color stm = pos->stm;
+	enum square from = MOVE_FROM(move), to = MOVE_TO(move);
+	u64 from_bb = BB_FROM_SQUARE(from);
+	u64 diagonal = pos->piece[BISHOP] | pos->piece[QUEEN];
+	u64 straight = pos->piece[ROOK] | pos->piece[QUEEN];
+	u64 occ = pos->piece[ALL_PIECES];
+	u64 attackers = pos_attackers(pos, to), allies;
+	enum piece_type lva = PIECE_TYPE(pos->board[from]);
+
+	gain[d] = mvv[PIECE_TYPE(pos->board[to])];
+	while (++d) {
+		gain[d] = mvv[lva] - gain[d - 1];
+		if (MAX(-gain[d - 1], gain[d]) < 0)
+			break;
+
+		occ ^= from_bb;
+		if (lva == PAWN || lva == BISHOP || lva == QUEEN)
+			attackers |= bb_attacks(BISHOP, to, occ) & diagonal;
+		if (lva == ROOK || lva == QUEEN)
+			attackers |= bb_attacks(ROOK, to, occ) & straight;
+		attackers &= occ;
+
+		allies = pos->color[stm ^= 1] & attackers;
+		if (!(allies & pos->color[stm]))
+			break;
+		for (lva = PAWN; lva <= KING; lva++) {
+			if ((from_bb = allies & pos->piece[lva]))
+				break;
+		}
+		from_bb &= -from_bb;
+	}
+
+	while (--d)
+		gain[d - 1] = -MAX(-gain[d - 1], gain[d]);
+
+	return gain[0];
+}
 
 void score_captures(struct move_picker *mp, struct position *pos)
 {
 	enum move *m;
 	for (m = mp->moves; m != mp->captures; m++)
-		mp->scores[m - mp->moves] =
-		    mvv[PIECE_TYPE(pos->board[MOVE_TO(*m)])];
+		mp->scores[m - mp->moves] = see(pos, *m);
 }
 
 void sort_moves(struct move_picker *mp, enum move *begin, enum move *end)
@@ -56,10 +100,11 @@ enum move mp_next(struct move_picker *mp, struct position *pos, bool skip_quiet)
 		mp->captures = mg_generate(MGT_CAPTURES, mp->moves, pos);
 		score_captures(mp, pos);
 		sort_moves(mp, mp->moves, mp->captures);
-		mp->stage = MP_STAGE_CAPTURES;
+		mp->stage = MP_STAGE_GOOD_CAPTURES;
 		[[fallthrough]];
-	case MP_STAGE_CAPTURES:
-		while (mp->captures != mp->moves) {
+	case MP_STAGE_GOOD_CAPTURES:
+		while (mp->captures != mp->moves &&
+		       mp->scores[mp->captures - mp->moves - 1] >= 0) {
 			bestmove = *--mp->captures;
 			if (bestmove == mp->hashmove)
 				continue;
@@ -93,6 +138,15 @@ enum move mp_next(struct move_picker *mp, struct position *pos, bool skip_quiet)
 			if (bestmove == mp->hashmove ||
 			    bestmove == mp->killer[0] ||
 			    bestmove == mp->killer[1])
+				continue;
+			return bestmove;
+		}
+		mp->stage = MP_STAGE_BAD_CAPTURES;
+		[[fallthrough]];
+	case MP_STAGE_BAD_CAPTURES:
+		while (mp->captures != mp->moves) {
+			bestmove = *--mp->captures;
+			if (bestmove == mp->hashmove)
 				continue;
 			return bestmove;
 		}
