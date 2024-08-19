73717879f56e3c8ec2eda627f4bcc2285a970be2
Rentib <sbitner420@tutanota.com>
2024-08-19 16:16:24 +0200

    feat(evaluate): trace

--- src/evaluate.c
+++ src/evaluate.c
@@ -13,7 +13,16 @@ struct eval_params eval_params = {
 		    S(901, 901), S(0, 0)},
 
     /* clang-format off */
-    .pawn_pcsqt = {0},
+    .pawn_pcsqt = {
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+    },
     .knight_pcsqt = {
 	    S(-15, -25), S(0, 0),  S(0, 0),  S(0, 0),  S(0, 0),  S(0, 0),  S(0, 0),  S(-15, -25),
 	    S(-10, -20), S(0, 0),  S(0, 0),  S(0, 0),  S(0, 0),  S(0, 0),  S(0, 0),  S(-10, -20),
@@ -34,8 +43,26 @@ struct eval_params eval_params = {
 	    S(0, 0), S(10, 5), S(0, 5),  S(7, 5),  S(7, 5),  S(0, 5),  S(10, 5), S(0, 0),
 	    S(0, 0), S(0, 0),  S(-6, 0), S(0, 0),  S(0, 0),  S(-8, 0), S(0, 0),  S(0, 0),
     },
-    .rook_pcsqt = {0},
-    .queen_pcsqt = {0},
+    .rook_pcsqt = {
+	    S(7, 12), S(4, 7), S(7, 1), S(-16, -4), S(-1, 7), S(3, 9), S(-6, -2), S(7, 4),
+	    S(5, -6), S(6, -16), S(8, -3), S(-8, 8), S(11, -16), S(5, 9), S(-3, -1), S(2, -4),
+	    S(-6, 4), S(2, -2), S(-6, -1), S(2, 11), S(7, 4), S(1, 1), S(13, 6), S(-16, -9),
+	    S(-3, -2), S(-9, 6), S(0, 3), S(-14, -4), S(1, 4), S(8, 5), S(2, -6), S(-3, -1),
+	    S(-5, -5), S(-4, -2), S(-8, -10), S(-1, -11), S(7, -17), S(-6, 4), S(-7, 2), S(-8, -1),
+	    S(-2,  5), S(-2, -3), S(-7, 2), S(4, 13), S(1, 0), S(1, 5), S(-1, -2), S(15, -8),
+	    S(-10, -2), S(-5, 4), S(-2, -6), S(-10, 2), S(1, 1), S(4, 0), S(5, -5), S(12, -9),
+	    S(7, -2), S(8, 1), S(-1, 4), S(1, 5), S(6, -1), S(4, -1), S(-10, 2), S(-4, -3),
+    },
+    .queen_pcsqt = {
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+	    S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0),
+    },
     .king_pcsqt = {
 	    S(-10, -50), S(-10, -20), S(-10, -20), S(-10, -20), S(-10, -20), S(-10, -20), S(-10, -20), S(-10, -50),
 	    S(-10, -10), S(-10, 0),   S(-10, 0),   S(-10, 0),   S(-10, 0),   S(-10, 0),   S(-10, 0),   S(-10, -10),
@@ -61,9 +88,16 @@ struct eval_params eval_params = {
 
     .knight_adj = {S(-9, -11), S(-8, -8), S(-5, -7), S(-4, -4), S(-1, -3),
 		    S(2, 0), S(0, 1), S(3, 4), S(10, 6)},
+    .knight_defended_by_pawn = S(1, 1),
     .knight_outpost = S(54, 31),
+    .knight_mobility = {S(0, 0), S(1, 1), S(2, 2), S(3, 3), S(4, 4), S(5, 5),
+                    S(6, 6), S(7, 7), S(8, 8)},
 
     .bishop_pair = S(20, 41),
+    .bishop_rammed_pawns = S(-2, -1),
+    .bishop_mobility = {S(0, 0), S(2, 1), S(4, 2), S(6, 3), S(8, 4), S(10, 5),
+                    S(12, 6), S(14, 7), S(16, 8), S(18, 9), S(20, 10),
+                    S(22, 11), S(24, 12), S(26, 13)},
 
     .rook_connected = S(8, 15),
     .rook_adj = {S(15, 14), S(13, 10), S(9, 9), S(8, 5), S(2, 4), S(0, -1),
@@ -71,13 +105,31 @@ struct eval_params eval_params = {
     .rook_open_file = S(20, 23),
     .rook_semiopen_file = S(11, 24),
     .rook_7th = S(8, 17),
+    .rook_mobility = {S(0, 0), S(1, 0), S(2, 0), S(3, 0), S(4, 0), S(5, 0),
+                    S(6, 0), S(7, 0), S(8, 0), S(9, 0), S(10, 0), S(11, 0),
+                    S(12, 0), S(13, 0), S(14, 0)},
 };
 static struct eval_params *ep = &eval_params;
+
+#ifdef TUNE
+struct eval_trace eval_trace;
+#define TRACE_RESET()                                                          \
+	eval_trace = (struct eval_trace) { 0 }
+#define TRACE_INC(field, color)          eval_trace.field[color]++
+#define TRACE_INC_VAL(field, color, val) eval_trace.field[color] += val
+#define TRACE_SET(field, val)            eval_trace.field = val
+#else
+#define TRACE_RESET()
+#define TRACE_INC(field, color)
+#define TRACE_PHASE(val)
+#endif
 
 static int eval_pawns(const struct position *position, const enum color side);
 static int eval_knights(const struct position *position, const enum color side);
 static int eval_bishops(const struct position *position, const enum color side);
 static int eval_rooks(const struct position *position, const enum color side);
+static int eval_queens(const struct position *position, const enum color side);
+static int eval_king(const struct position *position, const enum color side);
 
 static u64 mask_rank[8];
 static u64 mask_file[8];
@@ -101,12 +153,14 @@ int eval_pawns(const struct position *pos, const enum color side)
 		enemy_pawns = __builtin_bswap64(enemy_pawns);
 	}
 
-	// center
+	/* center */
 	mask = allied_pawns & mask_center_pawn;
 	eval += ep->pawn_center[BB_POPCOUNT(mask)];
+	TRACE_INC(pawn_center[BB_POPCOUNT(mask)], side);
 
 	for (mask = allied_pawns; mask;) {
 		sq = bb_poplsb(&mask);
+		TRACE_INC(piece_value[PAWN], side);
 
 		blocked = enemy_pawns & BB_FROM_SQUARE(sq + NORTH);
 		lever = enemy_pawns & bb_pawn_attacks(side, sq);
@@ -116,32 +170,48 @@ int eval_pawns(const struct position *pos, const enum color side)
 		stoppers = enemy_pawns & mask_passed[sq];
 		support = neighbours & mask_rank[SQ_RANK(sq + SOUTH)];
 
-		// backward
+		/* piece square table */
+		eval += ep->pawn_pcsqt[sq];
+		TRACE_INC(pawn_pcsqt[sq], side);
+
+		/* backward */
 		if (!(neighbours & (~BB_RANK_8 << 8 * SQ_RANK(sq)) &&
-		      (blocked | lever_push)))
+		      (blocked | lever_push))) {
 			eval += ep->pawn_backward;
+			TRACE_INC(pawn_backward, side);
+		}
 
-		// blocked
-		if (blocked && sq <= SQ_H5)
+		/* blocked */
+		if (blocked && sq <= SQ_H5) {
 			eval += ep->pawn_blocked[SQ_RANK(sq) - 2];
+			TRACE_INC(pawn_blocked[SQ_RANK(sq) - 2], side);
+		}
 
-		// doubled
-		if (allied_pawns & BB_FROM_SQUARE(sq + SOUTH))
+		/* doubled */
+		if (allied_pawns & BB_FROM_SQUARE(sq + SOUTH)) {
 			eval += ep->pawn_doubled;
+			TRACE_INC(pawn_doubled, side);
+		}
 
-		// connected
-		if (phalanx | support)
+		/* connected */
+		if (phalanx | support) {
 			eval += ep->pawn_connected[SQ_RANK(sq)];
+			TRACE_INC(pawn_connected[SQ_RANK(sq)], side);
+		}
 
-		// isolated
-		else if (!neighbours)
+		/* isolated */
+		else if (!neighbours) {
 			eval += ep->pawn_isolated;
+			TRACE_INC(pawn_isolated, side);
+		}
 
-		// passed
+		/* passed */
 		if (!(lever ^ stoppers) ||
 		    (!(stoppers ^ lever_push) &&
-		     BB_POPCOUNT(phalanx) >= BB_POPCOUNT(lever_push)))
+		     BB_POPCOUNT(phalanx) >= BB_POPCOUNT(lever_push))) {
 			eval += ep->pawn_passed[SQ_RANK(sq)];
+			TRACE_INC(pawn_passed[SQ_RANK(sq)], side);
+		}
 	}
 
 	return eval;
@@ -165,37 +235,43 @@ static int eval_knights(const struct position *pos, const enum color side)
 
 	for (; mask;) {
 		sq = bb_poplsb(&mask);
+		TRACE_INC(piece_value[KNIGHT], side);
 
-		// piece square table
+		/* piece square table */
 		eval += ep->knight_pcsqt[sq];
+		TRACE_INC(knight_pcsqt[sq], side);
 
-		// decrease value as allied pawns disappear
+		/* decrease value as allied pawns disappear */
 		eval += ep->knight_adj[pawn_cnt[side]];
+		TRACE_INC(knight_adj[pawn_cnt[side]], side);
 
 		if (bb_pawn_attacks(BLACK, sq) & allied_pawns) {
-			// TODO: add to eval_params
-			// knight defended by a pawn
-			eval += S(1, 1);
-			// outposts
+			/* knight defended by a pawn */
+			eval += ep->knight_defended_by_pawn;
+			TRACE_INC(knight_defended_by_pawn, side);
+			/* outposts */
 			if (((SQ_A6 <= sq && sq <= SQ_H5) ||
 			     (SQ_C4 <= sq && sq <= SQ_F4)) &&
 			    !(enemy_pawns & mask_passed[sq] &
-			      mask_adj_file[SQ_FILE(sq)]))
+			      mask_adj_file[SQ_FILE(sq)])) {
 				eval += ep->knight_outpost;
+				TRACE_INC(knight_outpost, side);
+			}
 		}
 
-		// TODO: add to eval_params
-		// mobility
-		eval +=
-		    S(BB_POPCOUNT(bb_attacks(KNIGHT, sq, 0) &
-				  ~(allies | bb_shift(enemy_pawns, SOUTH_EAST) |
-				    bb_shift(enemy_pawns, SOUTH_WEST))),
-		      BB_POPCOUNT(bb_attacks(KNIGHT, sq, 0) &
-				  ~(allies | bb_shift(enemy_pawns, SOUTH_EAST) |
-				    bb_shift(enemy_pawns, SOUTH_WEST))));
+		/* mobility */
+		eval += ep->knight_mobility[BB_POPCOUNT(
+		    bb_attacks(KNIGHT, sq, 0) &
+		    ~(allies | bb_shift(enemy_pawns, SOUTH_EAST) |
+		      bb_shift(enemy_pawns, SOUTH_WEST)))];
+		TRACE_INC(knight_mobility[BB_POPCOUNT(
+			      bb_attacks(KNIGHT, sq, 0) &
+			      ~(allies | bb_shift(enemy_pawns, SOUTH_EAST) |
+				bb_shift(enemy_pawns, SOUTH_WEST)))],
+			  side);
 
-		// TODO: knight trapped on A8/H8/A7/H7 or A1/H1/A2/H2
-		// TODO: penalty for an undefended minor piece
+		/* TODO: knight trapped on A8/H8/A7/H7 or A1/H1/A2/H2 */
+		/* TODO: penalty for an undefended minor piece */
 	}
 
 	return eval;
@@ -206,6 +282,7 @@ static int eval_bishops(const struct position *pos, const enum color side)
 	int eval = 0;
 
 	enum square sq;
+	(void)sq;
 	u64 mask = pos->color[side] & pos->piece[BISHOP];
 	u64 allies = pos->color[side];
 	u64 allied_pawns = pos->color[side] & pos->piece[PAWN];
@@ -220,38 +297,48 @@ static int eval_bishops(const struct position *pos, const enum color side)
 		occ = __builtin_bswap64(occ);
 	}
 
-	// bishop pair
-	if (BB_SEVERAL(mask))
+	/* bishop pair */
+	if (BB_SEVERAL(mask)) {
 		eval += ep->bishop_pair;
+		TRACE_INC(bishop_pair, side);
+	}
 
 	for (; mask;) {
 		sq = bb_poplsb(&mask);
+		TRACE_INC(piece_value[BISHOP], side);
 
-		// piece square table
+		/* piece square table */
 		eval += ep->bishop_pcsqt[sq];
+		TRACE_INC(bishop_pcsqt[sq], side);
 
-		// TODO: add to eval_params
-		// bad bishop
+		/* bad bishop */
 		if (sq % 2 == 0) {
-			eval -= S(BB_POPCOUNT(allied_pawns & BB_WHITE_SQUARES),
-				  BB_POPCOUNT(allied_pawns & BB_WHITE_SQUARES));
+			eval += ep->bishop_rammed_pawns *
+				BB_POPCOUNT(allied_pawns & BB_WHITE_SQUARES);
+			TRACE_INC_VAL(
+			    bishop_rammed_pawns, side,
+			    BB_POPCOUNT(allied_pawns & BB_WHITE_SQUARES));
 		} else {
-			eval -= S(BB_POPCOUNT(allied_pawns & BB_BLACK_SQUARES),
-				  BB_POPCOUNT(allied_pawns & BB_BLACK_SQUARES));
+			eval += ep->bishop_rammed_pawns *
+				BB_POPCOUNT(allied_pawns & BB_BLACK_SQUARES);
+			TRACE_INC_VAL(
+			    bishop_rammed_pawns, side,
+			    BB_POPCOUNT(allied_pawns & BB_BLACK_SQUARES));
 		}
 
-		// TODO: add to eval_params
-		// mobility
-		eval +=
-		    S(BB_POPCOUNT(bb_attacks(BISHOP, sq, occ) & ~allies) * 2,
-		      BB_POPCOUNT(bb_attacks(BISHOP, sq, occ) & ~allies) * 1);
+		/* mobility */
+		eval += ep->bishop_mobility[BB_POPCOUNT(
+		    bb_attacks(BISHOP, sq, occ) & ~allies)];
+		TRACE_INC(bishop_mobility[BB_POPCOUNT(
+			      bb_attacks(BISHOP, sq, occ) & ~allies)],
+			  side);
 
-		// TODO: bishop vs knight
-		// TODO: color weakness
-		// TODO: bishop trapped on A2/H2/A7/H7 or on A3/H3/A6/H6
-		// TODO: fianchetto
-		// TODO: returnign bishop
-		// TODO: penalty for an undefended minor piece
+		/* TODO: bishop vs knight */
+		/* TODO: color weakness */
+		/* TODO: bishop trapped on A2/H2/A7/H7 or on A3/H3/A6/H6 */
+		/* TODO: fianchetto */
+		/* TODO: returning bishop */
+		/* TODO: penalty for an undefended minor piece */
 	}
 
 	return eval;
@@ -264,40 +351,94 @@ static int eval_rooks(const struct position *pos, const enum color side)
 	u64 allies = pos->color[side];
 	u64 mask = allies & pos->piece[ROOK];
 
-	// connected rooks
+	/* connected rooks */
 	if (BB_SEVERAL(mask)) {
 		sq = bb_poplsb(&mask);
-		if (bb_attacks(ROOK, sq, pos->piece[ALL_PIECES]) & mask)
+		if (bb_attacks(ROOK, sq, pos->piece[ALL_PIECES]) & mask) {
 			eval += ep->rook_connected;
+			TRACE_INC(rook_connected, side);
+		}
 	}
 	mask = allies & pos->piece[ROOK];
 
 	for (; mask;) {
 		sq = bb_poplsb(&mask);
+		TRACE_INC(piece_value[ROOK], side);
 
-		// increasing value as pawns disappear
+		eval += side == WHITE ? ep->rook_pcsqt[sq]
+				      : ep->rook_pcsqt[SQ_FLIP(sq)];
+		TRACE_INC(rook_pcsqt[side == WHITE ? sq : SQ_FLIP(sq)], side);
+
+		/* increasing value as pawns disappear */
 		eval += ep->rook_adj[pawn_cnt[side]];
+		TRACE_INC(rook_adj[pawn_cnt[side]], side);
 
-		// (semi) open file
-		if (!(mask_file[SQ_FILE(sq)] & pos->piece[PAWN]))
+		/* (semi) open file */
+		if (!(mask_file[SQ_FILE(sq)] & pos->piece[PAWN])) {
 			eval += ep->rook_open_file;
-		else if (!(mask_file[SQ_FILE(sq)] & pos->piece[PAWN] & allies))
+			TRACE_INC(rook_open_file, side);
+		} else if (!(mask_file[SQ_FILE(sq)] & pos->piece[PAWN] &
+			     allies)) {
 			eval += ep->rook_semiopen_file;
+			TRACE_INC(rook_semiopen_file, side);
+		}
 
-		// rook on 7th
+		/* rook on 7th */
 		if ((side == WHITE && 8 - SQ_RANK(sq) >= 7) ||
-		    (side == BLACK && 8 - SQ_RANK(sq) <= 2))
+		    (side == BLACK && 8 - SQ_RANK(sq) <= 2)) {
 			eval += ep->rook_7th;
+			TRACE_INC(rook_7th, side);
+		}
 
-		// TODO: add to eval_params
-		// mobility
-		eval +=
-		    S(BB_POPCOUNT(bb_attacks(ROOK, sq, pos->piece[ALL_PIECES])),
-		      0);
+		/* mobility */
+		eval += ep->rook_mobility[BB_POPCOUNT(
+		    bb_attacks(ROOK, sq, pos->piece[ALL_PIECES]))];
+		TRACE_INC(rook_mobility[BB_POPCOUNT(
+			      bb_attacks(ROOK, sq, pos->piece[ALL_PIECES]))],
+			  side);
 
-		// TODO: Tarrasch rule
-		// TODO: penalty for being blocked by king that can't castle
+		/* TODO: Tarrasch rule */
+		/* TODO: penalty for being blocked by king that can't castle */
 	}
+
+	return eval;
+}
+
+static int eval_queens(const struct position *pos, const enum color side)
+{
+	int eval = 0;
+	enum square sq;
+	(void)sq;
+	u64 mask = pos->color[side] & pos->piece[QUEEN];
+
+	if (side == BLACK) {
+		mask = __builtin_bswap64(mask);
+	}
+
+	while (mask) {
+		sq = bb_poplsb(&mask);
+		TRACE_INC(piece_value[QUEEN], side);
+
+		eval += ep->queen_pcsqt[sq];
+		TRACE_INC(queen_pcsqt[sq], side);
+	}
+
+	return eval;
+}
+
+static int eval_king(const struct position *pos, const enum color side)
+{
+	int eval = 0;
+	enum square ksq;
+	u64 mask = pos->color[side] & pos->piece[KING];
+
+	ksq = BB_TO_SQUARE(mask);
+	TRACE_INC(piece_value[KING], side);
+	if (side == BLACK)
+		ksq = SQ_FLIP(ksq);
+
+	eval += ep->king_pcsqt[ksq];
+	TRACE_INC(king_pcsqt[ksq], side);
 
 	return eval;
 }
@@ -307,13 +448,14 @@ int evaluate(const struct position *pos)
 	int score, phase;
 	enum piece_type pt;
 	int eval = 0;
-	enum square ksq[COLOR_NB];
+
+	TRACE_RESET();
 
 	pawn_cnt[WHITE] = BB_POPCOUNT(pos->piece[PAWN] & pos->color[WHITE]);
 	pawn_cnt[BLACK] = BB_POPCOUNT(pos->piece[PAWN] & pos->color[BLACK]);
 
-	if (!pht_probe(pos->pawn_key, pos->color[WHITE] & pos->piece[PAWN],
-		       pos->color[BLACK] & pos->piece[PAWN], &eval)) {
+	if (1 || !pht_probe(pos->pawn_key, pos->color[WHITE] & pos->piece[PAWN],
+			    pos->color[BLACK] & pos->piece[PAWN], &eval)) {
 		eval += eval_pawns(pos, WHITE) - eval_pawns(pos, BLACK);
 		pht_store(pos->pawn_key, pos->color[WHITE] & pos->piece[PAWN],
 			  pos->color[BLACK] & pos->piece[PAWN], eval);
@@ -321,11 +463,8 @@ int evaluate(const struct position *pos)
 	eval += eval_knights(pos, WHITE) - eval_knights(pos, BLACK);
 	eval += eval_bishops(pos, WHITE) - eval_bishops(pos, BLACK);
 	eval += eval_rooks(pos, WHITE) - eval_rooks(pos, BLACK);
-
-	ksq[WHITE] = BB_TO_SQUARE(pos->color[WHITE] & pos->piece[KING]);
-	ksq[BLACK] = BB_TO_SQUARE(pos->color[BLACK] & pos->piece[KING]);
-	eval += ep->king_pcsqt[ksq[WHITE]];
-	eval -= ep->king_pcsqt[SQ_FLIP(ksq[BLACK])];
+	eval += eval_queens(pos, WHITE) - eval_queens(pos, BLACK);
+	eval += eval_king(pos, WHITE) - eval_king(pos, BLACK);
 
 	/* material */
 	for (pt = PAWN; pt <= KING; pt++) {
@@ -345,6 +484,9 @@ int evaluate(const struct position *pos)
 	phase -= BB_POPCOUNT(pos->piece[ROOK]) * 2;
 	phase -= BB_POPCOUNT(pos->piece[QUEEN]) * 4;
 	phase = (phase * 256 + 24 / 2) / 24;
+
+	TRACE_SET(eval, eval);
+	TRACE_SET(phase, phase);
 
 	score = ((SMG(eval) * (256 - phase)) + (SEG(eval) * phase)) / 256;
 
--- src/evaluate.h
+++ src/evaluate.h
@@ -1,10 +1,11 @@
 #ifndef KNUR_EVALUATE_H_
 #define KNUR_EVALUATE_H_
 
+#include "knur.h"
 #include "position.h"
 
 struct eval_params {
-	int piece_value[PIECE_TYPE_NB];
+	int piece_value[6];
 
 	int pawn_pcsqt[64];
 	int knight_pcsqt[64];
@@ -22,16 +23,62 @@ struct eval_params {
 	int pawn_center[6];
 
 	int knight_adj[9];
+	int knight_defended_by_pawn;
 	int knight_outpost;
+	int knight_mobility[9];
 
 	int bishop_pair;
+	int bishop_rammed_pawns;
+	int bishop_mobility[14];
 
 	int rook_connected;
 	int rook_adj[9];
 	int rook_open_file;
 	int rook_semiopen_file;
 	int rook_7th;
+	int rook_mobility[15];
 };
+
+#ifdef TUNE
+struct eval_trace {
+	int piece_value[6][COLOR_NB];
+
+	bool pawn_pcsqt[SQUARE_NB][COLOR_NB];
+	bool knight_pcsqt[SQUARE_NB][COLOR_NB];
+	bool bishop_pcsqt[SQUARE_NB][COLOR_NB];
+	bool rook_pcsqt[SQUARE_NB][COLOR_NB];
+	bool queen_pcsqt[SQUARE_NB][COLOR_NB];
+	bool king_pcsqt[SQUARE_NB][COLOR_NB];
+
+	int pawn_backward[COLOR_NB];
+	int pawn_blocked[2][COLOR_NB];
+	int pawn_doubled[COLOR_NB];
+	int pawn_connected[8][COLOR_NB];
+	int pawn_isolated[COLOR_NB];
+	int pawn_passed[8][COLOR_NB];
+	int pawn_center[6][COLOR_NB];
+
+	int knight_adj[9][COLOR_NB];
+	int knight_defended_by_pawn[COLOR_NB];
+	int knight_outpost[COLOR_NB];
+	int knight_mobility[9][COLOR_NB];
+
+	int bishop_pair[COLOR_NB];
+	int bishop_rammed_pawns[COLOR_NB];
+	int bishop_mobility[14][COLOR_NB];
+
+	int rook_connected[COLOR_NB];
+	int rook_adj[9][COLOR_NB];
+	int rook_open_file[COLOR_NB];
+	int rook_semiopen_file[COLOR_NB];
+	int rook_7th[COLOR_NB];
+	int rook_mobility[15][COLOR_NB];
+
+	int eval;
+	int phase;
+};
+extern struct eval_trace eval_trace;
+#endif
 
 int evaluate(const struct position *position);
 void evaluate_init(void);
